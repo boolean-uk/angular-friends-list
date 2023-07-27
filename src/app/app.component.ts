cb37935d0496d10a48f7a6532deff9953de79fb3
Wojciech Żółkowski <wojciechzolkowski@gmail.com>
2023-07-27 12:19:34 +0200

    Finished extenions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+
   newFriend: string | null = null;
   favorite: string | null = null;
-
+  favorites: string[] = []
   people: string[] = ['lewis', 'jules', 'ed', 'nathan', 'dave', 'nigel'];
 
   addFriend() {
     if (!this.newFriend) {
+      console.log(this.newFriend)
       return;
     }
+    console.log(this.newFriend)
     this.people.push(this.newFriend);
     this.newFriend = null;
   }
+
   OnsetFavorite(person: string) {
-    this.favorite= person
+    this.favorites.push(person)
   }
+
+  removeFavorite(favorite: string) {
+    this.favorites = this.favorites.filter(person => person != favorite)
+    }
 
 }
