7afce6a6f106a5439d44d585523725ae35dc9392
Marcin Stefanowicz <marcin.stefanowicz@talan.com>
2023-07-27 11:39:03 +0200

    displaying people works

---
+++ src/app/people/people.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-people',
+  templateUrl: './people.component.html',
+  styleUrls: ['./people.component.css']
+})
+export class PeopleComponent {
+  @Input() person: string = ''
+}
