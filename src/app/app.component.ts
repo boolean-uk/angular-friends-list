df4a6a6ad2b1aa3f4d9a51e7f57c06f823f64cef
Enock97 <enockladu@hotmail.com>
2024-03-11 13:35:45 +0100

    "Completed core and extension exercise"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PeopleComponent } from './people/people.component';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   newFriend: string | null = null;
-  favorite: string | null = null;
+  favorites: string[] = [];
 
   people: string[] = ['lewis', 'jules', 'ed', 'nathan', 'dave', 'nigel'];
 
@@ -18,4 +19,19 @@ export class AppComponent {
     this.people.push(this.newFriend);
     this.newFriend = null;
   }
+
+   addFavorite(person: string) {
+        if (!this.favorites.includes(person)) {
+            this.favorites.push(person);
+        }
+        console.log(this.favorites)
+    };
+
+  removeFavorite(person: string) {
+    let index = this.favorites.indexOf(person);
+        if (index > -1) {
+            this.favorites.splice(index, 1);
+        }
+    };
+    
 }
