905e8b1e0aa5725ede8fce53b09b240506b9f0ce
spectraldesign <spectraldesign1@gmail.com>
2024-03-11 11:25:55 +0100

    Core + Extensions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   newFriend: string | null = null;
-  favorite: string | null = null;
-
+  favourites: string[] = [];
   people: string[] = ['lewis', 'jules', 'ed', 'nathan', 'dave', 'nigel'];
 
   addFriend() {
@@ -17,5 +16,21 @@ export class AppComponent {
     }
     this.people.push(this.newFriend);
     this.newFriend = null;
+  }
+
+  addFavourite(person: string) {
+    this.favourites.push(person);
+  }
+
+  removeFavourite(person: string) {
+    this.favourites = this.favourites.filter((favourite) => favourite !== person);
+  }
+
+  handleFavouriteButtonClick(event: { person: string }) {
+    if (this.favourites.includes(event.person)) {
+      this.removeFavourite(event.person);
+    } else {
+      this.addFavourite(event.person);
+    }
   }
 }
