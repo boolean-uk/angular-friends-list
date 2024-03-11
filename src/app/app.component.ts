3dee8e00f00f8defe27a13754964512e1b0d48e8
Alexander Niklasson <alexander.niklasson@live.com>
2024-03-11 13:34:44 +0100

    core & extension complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   favorite: string | null = null;
 
   people: string[] = ['lewis', 'jules', 'ed', 'nathan', 'dave', 'nigel'];
+  favorites: string[] = [];
 
   addFriend() {
     if (!this.newFriend) {
@@ -17,5 +18,16 @@ export class AppComponent {
     }
     this.people.push(this.newFriend);
     this.newFriend = null;
+  }
+  setFavorite(friend: string) {
+    this.favorite = friend;
+    if (!this.favorites.includes(this.favorite)) {
+      this.favorites.push(this.favorite);
+      this.favorite = null;
+    }
+  }
+
+  removeFavorite(favorite: string) {
+    this.favorites = this.favorites.filter((f) => f !== favorite);
   }
 }
