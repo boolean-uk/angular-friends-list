057f6d0a8941f01ab625b235e04a51df53b0b903
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-03-11 14:31:19 +0100

    Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,28 @@ import { Component, Input } from '@angular/core';
 export class AppComponent {
 handleFavouriteClicked(personName: string) {
   this.favorite = personName;
+  if(!this.favourites?.includes(personName)) {
+    this.favourites?.push(personName)
+    console.log("asdad")
+  }
+
 }
+
+handleFavoriteRemove(personName: string) {
+  if(this.favorite === personName) {
+    this.favorite = "";
+  }
+
+  if(this.favourites.includes(personName)) {
+    this.favourites = this.favourites.filter(fave => fave !== personName);
+  }
+}
+
+
   newFriend: string | null = null;
   favorite: string | null = null;
+
+  favourites: string[] = []
 
   people: string[] = ['lewis', 'jules', 'ed', 'nathan', 'dave', 'nigel'];
 
--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -6,12 +6,17 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./people.component.css']
 })
 export class PeopleComponent {
+  removeFavorite(person: string) {
+    this.favoriteRemove.emit(person);
+  }
   @Input("person") person: string = "Bob";
 
   @Output("favourite") favourite = new EventEmitter<string>();
 
+  @Output("favoriteRemove") favoriteRemove = new EventEmitter<string>();
+
   handleFavouriteChange(person: string) {
-    
+
     this.favourite.emit(person)
   }
 }
