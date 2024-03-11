b9940ef8aae6c1539ba5e9e6d6b39126ec5e747f
Elias <elias.experis@se.experis.com>
2024-03-11 12:22:43 +0100

    core and extension done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app-component/app.component';
+import { PeopleComponent } from './people-component/people.component';
 import { FormsModule } from '@angular/forms';
-
+import { CommonModule } from '@angular/common';
+import { FavoritePeopleComponent } from './favorite-people/favorite-people.component';
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, PeopleComponent, FavoritePeopleComponent],
+  imports: [BrowserModule, FormsModule, CommonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
