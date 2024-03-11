620885a46ea8680d517a862bfdb5a1abb3198319
Sini Suihkonen <sahsuihkonen@gmail.com>
2024-03-11 13:15:47 +0200

    core + extension

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html', // like the jsx file in react
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
@@ -10,6 +10,18 @@ export class AppComponent {
   favorite: string | null = null;
 
   people: string[] = ['lewis', 'jules', 'ed', 'nathan', 'dave', 'nigel'];
+  favorites: string[] = [];
+
+  onSetFavorite(name: string): void {
+    this.favorite = name;
+    this.favorites.push(name);
+  }
+
+  onRemoveFavorite(name: string): void {
+    this.favorites = this.favorites.filter(function (item) {
+      return item !== name;
+    });
+  }
 
   addFriend() {
     if (!this.newFriend) {
