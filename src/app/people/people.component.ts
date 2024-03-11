c7cef7488325c1a980456365bc9a32148e2bc9f5
Nicolai Klokmose <angryniczorn@gmail.com>
2024-03-11 12:45:45 +0100

    core done

---
+++ src/app/people/people.component.ts
@@ -0,0 +1,11 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-people',
+  templateUrl: './people.component.html',
+  styleUrls: ['./people.component.css']
+})
+export class PeopleComponent {
+  @Input('person') person: string = "Rouge"
+  @Output('favoritted') favoritted = new EventEmitter<string>();
+}
