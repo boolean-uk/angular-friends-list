d0d225b03ff20808c22d76a0aec5a2833f5bfcb6
Caroline Berg <caroline.berg@no.experis.com>
2024-03-11 14:38:39 +0100

    Angular components - input and outputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  standalone: false,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  newFriend: string | null = null;
-  favorite: string | null = null;
+  @Input()newFriend: string | null = null;
+ 
+  favorites: string[] =[];
 
-  people: string[] = ['lewis', 'jules', 'ed', 'nathan', 'dave', 'nigel'];
+  people: string[] = ['Lewis', 'Jules', 'Ed', 'Nathan', 'Dave', 'Nigel'];
 
   addFriend() {
     if (!this.newFriend) {
       return;
     }
+    
     this.people.push(this.newFriend);
+    
+    console.log(this.people)
     this.newFriend = null;
   }
+
+  setFavourite(n: string) {
+    let element = this.favorites.filter(x=>x.includes(n))
+    if(element.length!==0){
+    }else{
+      
+    if(this.favorites.length >0){
+      this.favorites.push(', '+n);
+    }else{
+      this.favorites.push(n);
+    }
+    }
+  }
+
+  removeName(n:string){
+    this.favorites = this.favorites.filter(x=>!x.includes(n))
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { PeopleComponent } from 'src/people/people.component';
+import { FavouritesComponent } from 'src/favourites/favourites.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent,PeopleComponent, FavouritesComponent],
+  imports: [BrowserModule, FormsModule, CommonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
