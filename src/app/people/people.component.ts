8982d3bb949ae18432bf1831ddc115b291684fef
Maha Riazi <maha.riazi@hotmail.no>
2024-03-11 12:23:59 +0100

    core complete

---
+++ src/app/people/people.component.ts
@@ -0,0 +1,11 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-people',
+  templateUrl: './people.component.html',
+  styleUrls: ['./people.component.css'],
+})
+export class PeopleComponent {
+  @Input() 'name': string;
+  @Output('favourited') favourited = new EventEmitter<string>();
+}
