8f274174819c784f936147fa5a79d45042e64516
Knut S Rostad <knut.rostad@no.experis.com>
2024-03-11 15:06:36 +0100

    All done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   newFriend: string | null = null;
   favorite: string | null = null;
+  favouriteList: string[] = [];
 
   people: string[] = ['lewis', 'jules', 'ed', 'nathan', 'dave', 'nigel'];
 
@@ -19,7 +20,23 @@ export class AppComponent {
     this.newFriend = null;
   }
 
+  removeFriend(person: string) {
+    this.favouriteList = this.favouriteList.filter(
+      (favoritePerson) => favoritePerson !== person
+    );
+    if (this.favorite === person) {
+      this.favorite = null;
+    }
+  }
+
   favouritise(person: string) {
+    //Set person as favorite person
     this.favorite = person;
+
+    //Add the favourite to the list
+    if (this.favouriteList.includes(this.favorite)) {
+      return;
+    }
+    this.favouriteList.push(this.favorite);
   }
 }
