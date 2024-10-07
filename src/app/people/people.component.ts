b17718c596cb10eafd4ad5ea579b2af12e9c656d
John Ferdie Abueg <john.abueg@gmail.com>
2024-10-07 11:18:19 +0200

    added unfriend and conditional rendering

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -11,4 +11,5 @@ export class PeopleComponent {
   @Input('name') name: string = '';
   @Output('favorite') favorite = new EventEmitter<string>;
   @Output('removeFavorite') removeFavorite = new EventEmitter<string>;
+  @Output('unfriend') unfriend = new EventEmitter<string>;
 }
