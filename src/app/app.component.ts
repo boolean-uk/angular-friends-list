871d19606f51af8c91132ad896d6368ea6b11058
Kaja Thoresen Plaszko <kaja.plaszko@no.experis.com>
2024-10-07 16:29:54 +0200

    Kaja Plaszko

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   newFriend: string | null = null;
-  favorite: string | null = null;
+  favourites: string[] = [];
 
   people: string[] = ['lewis', 'jules', 'ed', 'nathan', 'dave', 'nigel'];
 
   addFriend() {
     if (!this.newFriend) {
       return;
+    } else if (!this.people.includes(this.newFriend)) {
+      this.people.push(this.newFriend);
     }
-    this.people.push(this.newFriend);
+    
     this.newFriend = null;
+  }
+
+  removeFriend(name: string) {
+    if (this.people.includes(name)) {
+      this.people.splice(this.people.indexOf(name), 1);
+
+      if (this.favourites.includes(name)) {
+        this.removeFavourite(name)
+      }
+    }
+  }
+
+  addFavourite(name: string) {
+    if (!this.favourites.includes(name)) {
+      this.favourites.push(name)
+    }
+    
+    return;
+  }
+
+  removeFavourite(name: string) {
+    if (this.favourites.includes(name)) {
+      this.favourites.splice(this.favourites.indexOf(name), 1);
+    }
+
+    return;
   }
 }
