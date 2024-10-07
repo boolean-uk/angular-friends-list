73dc0b4a78f50636263b85534cc7ea00822395f2
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-10-07 15:04:09 +0200

    Extension finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { PeopleComponent } from './people/people.component';
+import { FavoriteComponent } from "./favorite/favorite.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, PeopleComponent],
+  imports: [BrowserModule, FormsModule, PeopleComponent, FavoriteComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
