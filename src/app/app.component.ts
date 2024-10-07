72e3c9800c1ecd9856110443b78dd4990d826eef
Julia <julia.r.lindgren@gmail.com>
2024-10-07 14:16:01 +0200

    Complete core + extensions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   favorite: string | null = null;
 
   people: string[] = ['lewis', 'jules', 'ed', 'nathan', 'dave', 'nigel'];
+  favourites: string[] = [];
 
   addFriend() {
     if (!this.newFriend) {
@@ -18,4 +19,31 @@ export class AppComponent {
     this.people.push(this.newFriend);
     this.newFriend = null;
   }
+
+  removeFriend(name: string) {
+    if (this.people.includes(name)) {
+      this.people.splice(this.people.indexOf(name), 1);
+
+      if (this.favourites.includes(name)) {
+        this.removeFavourite(name)
+      }
+    }
+  }
+
+  addFavourite(name: string) {
+    if (!this.favourites.includes(name)) {
+      this.favourites.push(name)
+    }
+
+    return;
+  }
+
+  removeFavourite(name: string) {
+    if (this.favourites.includes(name)) {
+      this.favourites.splice(this.favourites.indexOf(name), 1);
+    }
+
+    return;
+  }
+
 }
