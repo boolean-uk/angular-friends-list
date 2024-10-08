884a2cd7fdc83bad04a17794b2f689e8ef5760f0
tvaltn <valtniemi@gmail.com>
2024-10-08 11:07:52 +0200

    core done

---
+++ src/app/people/people.component.ts
@@ -0,0 +1,13 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-people',
+  standalone: true,
+  imports: [],
+  templateUrl: './people.component.html',
+  styleUrl: './people.component.css'
+})
+export class PeopleComponent {
+  @Input('name') name: string = '';
+  @Output('favorite') favorite = new EventEmitter<string>();
+}
