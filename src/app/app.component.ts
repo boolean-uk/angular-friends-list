f103639b1df3c38faf22f26e4fc26bf2993ce7a6
Jonathan van Oudheusden <jonathanvanoudheusden@gmail.com>
2025-09-05 14:20:13 +0000

    core + extension

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+// import { PeopleComponent } from './people/people.component'; 
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  // standalone: true,
+  // imports: [PeopleComponent],
 })
 export class AppComponent {
   newFriend: string | null = null;
-  favorite: string | null = null;
+  favorites: string[] =  [ ];
 
   people: string[] = ['lewis', 'jules', 'ed', 'nathan', 'dave', 'nigel'];
 
@@ -17,5 +21,14 @@ export class AppComponent {
     }
     this.people.push(this.newFriend);
     this.newFriend = null;
+  }
+  addFavorite(favorite: string) {
+    if(!this.favorites.includes(favorite)) {
+      this.favorites.push(favorite);
+      // this.favorites.set([...current, name]);
+    }
+  }
+  remove(favorite: string) {
+    this.favorites = this.favorites.filter(fav => fav !== favorite);
   }
 }
